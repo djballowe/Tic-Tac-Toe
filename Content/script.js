20941c8e5d9c99e61243ae4ca269586c8ad6d8f9
djballowe <djballowe@gmail.com>
2022-05-30 15:50:12 -0700

    shortening the code for the button listners

--- Content/script.js
+++ Content/script.js
@@ -47,20 +47,20 @@ buttonListener.forEach((button) => {
         const player = document.getElementById('menu');
         const game = document.getElementById('tic');
         const nameEntry = document.getElementById('entry');
-        if (button.id === 'player' || button.id === 'computer') {
-            // player function whatever
-            player.style.display = 'none';
-            nameEntry.style.display = 'block';
-        } else if (button.id === 'play-again') {
-            // reset the game whatever
-        } else if (button.id === 'back') {
-            // reset the game whatever
-            player.style.display = 'block';
-            game.style.display = 'none';
-        } else if (button.id === 'submit') {
-            // start the game whatever
-            nameEntry.style.display = 'none';
-            game.style.display = 'block';
+        const computer = document.getElementById('computer')
+        switch (button.id) {
+            case 'player':
+                player.style.display = 'none';
+                nameEntry.style.display = 'block';
+            case 'computer':
+                player.style.display = 'none';
+                computer.style.display = 'block';
+            case 'submit':
+                game.style.display = 'block';
+                nameEntry.style.display = 'none';
+            case 'back':
+                location.reload();
+
         }
     })
 })
