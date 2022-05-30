ef1f2a0e9008a1c313b61f0ee4d07e9508b539ed
djballowe <djballowe@gmail.com>
2022-05-30 14:34:55 -0700

    finished the styles for the player entry selection

--- Content/script.js
+++ Content/script.js
@@ -46,16 +46,21 @@ buttonListener.forEach((button) => {
     button.addEventListener('click', () => {
         const player = document.getElementById('menu');
         const game = document.getElementById('tic');
+        const nameEntry = document.getElementById('entry');
         if (button.id === 'player' || button.id === 'computer') {
             // player function whatever
             player.style.display = 'none';
-            game.style.display = 'block';
+            nameEntry.style.display = 'block';
         } else if (button.id === 'play-again') {
             // reset the game whatever
         } else if (button.id === 'back') {
             // reset the game whatever
             player.style.display = 'block';
             game.style.display = 'none';
+        } else if (button.id === 'submit') {
+            // start the game whatever
+            nameEntry.style.display = 'none';
+            game.style.display = 'block';
         }
     })
 })
