f2f3e97e4cfffac46b192d2f3cb30398f6300cbf
djballowe <djballowe@gmail.com>
2022-06-02 11:20:42 -0700

    creating winning combinations and checking the board

--- Content/script.js
+++ Content/script.js
@@ -30,6 +30,18 @@ function stateOfPlay() {
                   0, 0, 0,
                   0, 0, 0]
 
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [0, 4, 6]
+    ]
+
+
     const board = (index, type) => {
     
         if (type === 'X') {
@@ -37,18 +49,19 @@ function stateOfPlay() {
         } else if (type === 'O') {
             game[index] = 2;
         }
-        console.log(game);
+        winCheck(game);
     }
 
     
 
     function winCheck() {
-        console.log(board);
+        return winningCombos.some(combination => {
+            return combination.every(index => {
+                
+            })
+        })
     }
     
-
-
-
     const divFactory = (type, attributes, text) => {
         const create = document.createElement(type);
         create.textContent = text;
